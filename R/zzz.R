c962824cc4d8dcf23223487e561c916ec173361d
Uri Simonsohn <56013245+urisohn@users.noreply.github.com>
2025-11-23 06:38:07 +0100

    export message.col

---
+++ R/zzz.R
@@ -0,0 +1,23 @@
+# Package initialization
+# This file is executed when the package is loaded
+
+.onLoad <- function(libname, pkgname) {
+  # Package initialization code goes here
+  # Currently no initialization needed, but this file is here for future use
+  invisible()
+}
+
+.onAttach <- function(libname, pkgname) {
+
+     #Startup msgs
+      #Version and URL for website
+          packageStartupMessage ("Attached: 'sohn' (Version: ",packageVersion('sohn'),  ")") 
+          
+      #While developing:
+         packageStartupMessage ("#######################################################\n",
+                              "This Version 2025 11 23 - 06.37AM\n" )
+
+
+  invisible()
+}
+
